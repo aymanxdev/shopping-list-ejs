38ae4747d07c3546051e17f5d79c3383c6a3bb75
aymanxdev <ayman.bacc@gmail.com>
2021-03-04 22:46:59 +0000

    complete weather api setup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 
 const express = require("express");
+const https = require("https");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
@@ -148,6 +149,37 @@ app.post("/delete", (req, res) => {
   }
 });
 
+//weather API request //
+app.post("/weather", (req, res) => {
+  const query = req.body.cityName;
+  const apiKey = process.env.APP_API_KEY;
+  const uri =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apiKey +
+    "&units=metric";
+
+  https.get(uri, (response) => {
+    console.log(response.statusCode);
+    response.on("data", (data) => {
+      const weatherData = JSON.parse(data);
+      const desc = weatherData.weather[0].description;
+      const temp = weatherData.main.temp;
+      const icon = weatherData.weather[0].icon;
+      const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.render("list", {
+        city: query,
+        weatherDay: desc,
+        temperature: temp,
+      });
+    });
+  });
+});
+
+// app.get("/", (req, res) => {
+//   res.render("list", { city: query, weatherDa: desc, temperature: temp });
+// });
 app.get("/about", function (req, res) {
   res.render("about");
 });
